93aaf636303acab5df801b4f0c6640a9c8cbc273
Tim <trmchale@gmail.com>
2015-09-07 16:25:49 -0400

    added c intro

--- server.js
+++ server.js
@@ -1,26 +1,7 @@
 var express = require('express');
 var app = express();
 
-var loadtest = require('loadtest');
- 
-function statusCallback(result) {
-    console.log('Current Status: ' + require('util').inspect(result));
-}
- 
-var options = {
-    url: 'http://localhost:3002',
-    maxRequests: 10000,
-    statusCallback: statusCallback
-};
- 
-loadtest.loadTest(options, function(error) {
-    if (error) {
-        return console.error('Got an error: %s', error);
-    }
-    console.log('Tests run successfully');
-});
-
-app.get('/', function (req, res) {
+app.get('/prefix-postfix', function (req, res) {
   res.sendFile(__dirname + '/main.html');
 });
 
@@ -28,10 +9,14 @@ app.get('/stacks', function(req, res) {
   res.sendFile(__dirname + '/stacks.html')
 });
 
+app.get('/', function(req,res) {
+  res.sendFile(__dirname + '/pointers_in_C.html')
+});
+
 var server = app.listen(3002, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
-  console.log(loadtest.loadTest);
+
 });
